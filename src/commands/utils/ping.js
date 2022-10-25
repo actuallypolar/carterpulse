8c09bf9f9e0f4f55ef3cd13b245bafb9279a4765
actuallypolar <growlemons@gmail.com>
2022-10-25 21:02:38 +0000

    init

---
+++ src/commands/utils/ping.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, GuildMember } = require("discord.js");
+const { execute } = require("../../events/client/ready");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Returns the ping of the bot.'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        const clientPing = message.createdTimestamp - interaction.createdTimestamp
+
+        const newMessage = `**API Latency**: \`${client.ws.ping}ms\`\n**Client Latency:** \`${clientPing}ms\``
+        await interaction.editReply({
+            content: newMessage
+        })
+    }
+}
---
+++ src/components/buttons/nickname-change.js
@@ -0,0 +1,10 @@
+module.exports = {
+    data: {
+        name: `nickname-change`
+    },
+        async execute(interaction, client) {
+            await interaction.reply({
+                content: `placeholder`
+            });
+    }
+}
