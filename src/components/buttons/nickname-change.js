2b9a6ec00fa1396d945359414141ddbff91f956f
actuallypolar <growlemons@gmail.com>
2022-11-05 20:13:52 +0000

    formatting

--- src/components/buttons/nickname-change.js
+++ src/components/buttons/nickname-change.js
@@ -2,9 +2,9 @@ module.exports = {
     data: {
         name: `nickname-change`
     },
-        async execute(interaction, client) {
-            await interaction.reply({
-                content: `placeholder`
-            });
+    async execute(interaction, client) {
+        await interaction.reply({
+            content: `placeholder`
+        });
     }
 }
